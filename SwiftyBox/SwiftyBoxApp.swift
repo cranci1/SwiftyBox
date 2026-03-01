4d5c0efbcaa1bff2fe135e8caea7ba8a6bfbdc98
cranci1 <100066266+cranci1@users.noreply.github.com>
2026-03-01 17:44:27 +0100

    First build

---
+++ SwiftyBox/SwiftyBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyBoxApp.swift
+//  SwiftyBox
+//
+//  Created by Francesco on 01/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
